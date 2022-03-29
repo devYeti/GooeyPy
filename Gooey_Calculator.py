2c150bd87804b6cccff1dad7098f9b07488e57f7
David Wakeley <devyeti1337@gmail.com>
2022-03-29 17:24:37 -0600

    created number entries with labels

--- Gooey_Calculator.py
+++ Gooey_Calculator.py
@@ -4,3 +4,8 @@
 from tkinter import *
 import tkinter.font as font
 
+root = Tk()
+num1 = Label(root, text = 'Enter first number:', font = ('Serif', 12))
+enter1 = Entry(root)
+num2 = Label(root, text = 'Enter second number:', font = ('Serif', 12))
+enter2 = Entry(root)
