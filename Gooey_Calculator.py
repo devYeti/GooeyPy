c1d51f447dd7eb09012f72917121b6d1840bdc73
David Wakeley <devyeti1337@gmail.com>
2022-03-29 17:39:57 -0600

    added result label

--- Gooey_Calculator.py
+++ Gooey_Calculator.py
@@ -17,9 +17,18 @@ sub = Button(root, text = 'Sub', width = 10, command = sub)
 mul = Button(root, text = 'Mul', width = 10, command = mul)
 div = Button(root, text = 'Div', width = 10, command = div)
 
+result = Label(root)
+
 
 # Grid locations:
 num1.grid(row = 0, column = 0)
 enter1.grid(row = 0, column = 1)
 num2.grid(row =1, column = 0)
 enter2.grid(row = 1, column = 1)
+add.grid(row = 2, column = 0)
+sub.grid(row = 3, column = 0)
+mul.grid(row = 2, column = 1)
+div.grid(row = 3, column = 1)
+result.grid(row = 4, column = 0)
+
+root.mainloop()
