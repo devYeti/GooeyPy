d5ad32d0e8d44802ed2fa4dfc3b0e4cb4c8d96d7
David Wakeley <devyeti1337@gmail.com>
2022-03-29 14:12:21 -0600

    created and placed several widgets.

--- First_Gooey.py
+++ First_Gooey.py
@@ -1,8 +1,24 @@
 # First project to implement a GUI
 # Working along with the Programming Hero app
+# David Wakeley github.com/devYeti 
 
 from tkinter import *
 
 # create app window
-root = Tk()
+root = Tk()  # Create Tk() class object 
+root.title('Ooohy Gooey!') # Title for root window
+root.geometry('500x200') # define window size 
+
+# Create widget objects: 
+course = Label(root, text = 'Programming Hero Course', bg = 'red', fg = 'white')
+enroll = Button(root, text = 'Enroll Now',fg = 'blue', bg = 'yellow')
+lbl = Label(root, text = 'Name')
+data = Entry(root)
+
+# Place widgets in root window:
+course.grid(row = 0, column = 1)
+enroll.grid(row = 1, column = 1)
+lbl.grid(row = 3, column = 0)
+data.grid(row = 3, column = 1)
+
 root.mainloop()
