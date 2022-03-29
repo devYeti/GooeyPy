fb270fedc0e310fd94bbe4936cebf59b71389dba
David Wakeley <devyeti1337@gmail.com>
2022-03-29 15:59:50 -0600

    Added python logo image with label

--- First_Gooey.py
+++ First_Gooey.py
@@ -3,6 +3,7 @@
 # David Wakeley github.com/devYeti 
 
 from tkinter import *
+from PIL import ImageTk, Image
 
 # create app window
 root = Tk()  # Create Tk() class object 
@@ -15,10 +16,18 @@ enroll = Button(root, text = 'Enroll Now',fg = 'blue', bg = 'yellow')
 lbl = Label(root, text = 'Name')
 data = Entry(root)
 
+# open image and create object:
+img_lbl = Label(root, text = 'Python')
+img = Image.open('python_logo.png')
+picture = ImageTk.PhotoImage(img)
+python = Label(root, image = picture)
+
 # Place widgets in root window:
-course.grid(row = 0, column = 1)
-enroll.grid(row = 1, column = 1)
-lbl.grid(row = 3, column = 0)
-data.grid(row = 3, column = 1)
+img_lbl.grid(row = 0, column = 2)
+python.grid(row = 1, column = 2)
+course.grid(row = 2, column = 1)
+enroll.grid(row = 3, column = 1)
+lbl.grid(row = 4, column = 0)
+data.grid(row = 4, column = 1)
 
 root.mainloop()
